0f2c6e19415bbcf14b1d4848a226fc8ebf4c4b45
Christopher De Cairos <cade@mozillafoundation.org>
2015-11-16 14:05:15 -0500

    Check event type

--- stripe.js
+++ stripe.js
@@ -10,8 +10,21 @@ Configure stripe to only hit this endpoint with the following events:
 - charge.updated
 */
 
+const allowed_events = [
+  "charge.succeeded",
+  "charge.captured",
+  "charge.failed",
+  "charge.refunded",
+  "charge.updated"
+];
+
 module.exports = function(request, reply) {
   var event = request.payload;
+
+  if (allowed_events.indexOf(event.type.toLowerCase()) === -1 {
+    return reply("Event type not supported");
+  }
+
   return process_charge(event.data.object, function(err) {
     if (err) {
       return reply(boom.wrap(err));
